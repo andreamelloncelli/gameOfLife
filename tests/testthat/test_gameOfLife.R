7e0d9f5b2b275e772d6c6ff3227b3f3d4bb3a126
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2018-03-24 18:38:33 +0100

    tests implemented

--- tests/testthat/test_gameOfLife.R
+++ tests/testthat/test_gameOfLife.R
@@ -1,30 +1,57 @@
+context("evolve_function")
 
+alive <- "alive"
+dead  <- "dead"
 
 test_that(desc = paste("Any live cell",
                        "with fewer than two live neighbours",
                        "dies,",
                        "as if caused by underpopulation."),
           code = {
-              
+              state <- alive
+
+              evolved_state_0 <- evolve(state, neigb = 0)
+              evolved_state_1 <- evolve(state, neigb = 1)
+
+              expect_equal(evolved_state_0, dead)
+              expect_equal(evolved_state_1, dead)
           })
+
 test_that(desc = paste("Any live cell",
                        "with two or three live neighbours",
                        "lives",
                        "on to the next generation."),
           code = {
-              
+              state <- alive
+
+              evolved_state_2 <- evolve(state, neigb = 2)
+              evolved_state_3 <- evolve(state, neigb = 3)
+
+              expect_equal(evolved_state_2, alive)
+              expect_equal(evolved_state_3, alive)
           })
+
 test_that(desc = paste("Any live cell",
                        "with more than three live neighbours",
                        "dies,",
                        "as if by overpopulation."),
           code = {
-              
+              state <- alive
+
+              evolved_state <- evolve(state, neigb = 4)
+
+              expect_equal(evolved_state, dead)
           })
+
 test_that(desc = paste("Any dead cell",
                        "with exactly three live neighbours",
                        "becomes a live cell,",
                        "as if by reproduction."),
           code = {
-              
+              state <- dead
+
+              evolved_state <- evolve(state, neigb = 3)
+
+              expect_equal(evolved_state, alive)
           })
+
